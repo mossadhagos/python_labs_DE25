58db3aa05c763bb19c314afab759571bf64b585d
mossadhagos <155261758+mossadhagos@users.noreply.github.com>
2025-10-27 23:21:42 +0100

    Update lab_02.py

--- python_lab_02/lab_02.py
+++
@@ -1,9 +0,0 @@
-from numbers import Number
-
-class Geometry:
-    def __init__(self,cerkel, rectangle ):
-        self.cerkel = cerkel
-        self.rectangle = rectangle
-        
-    def numbers(self, numbers: int | float):
-        self.numbers = numbers
