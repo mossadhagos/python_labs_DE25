d5831cea49790ed342cb90b5097066e4a800b5bc
mossadhagos <155261758+mossadhagos@users.noreply.github.com>
2025-10-31 21:14:25 +0100

    Update rectangle.py

---
+++ python_lab_02/rectangle.py
@@ -0,0 +1,20 @@
+from numbers import Number 
+
+class Rectangle:
+    
+    def __init__(self):
+        pass
+    
+    @property
+    def heihgt(self):
+        pass
+    
+    @property
+    def width(self):
+        pass 
+    
+    @property
+    def area(self):
+        pass
+    
+    
