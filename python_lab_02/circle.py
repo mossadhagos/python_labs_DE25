4f091265406de16b5f70a8633225bc6fac965a1b
mossadhagos <155261758+mossadhagos@users.noreply.github.com>
2025-10-31 21:14:20 +0100

    Update circle.py

---
+++ python_lab_02/circle.py
@@ -0,0 +1,24 @@
+from numbers import Number 
+
+class Circle:
+    
+    def __init__(self):
+        pass
+    
+    
+    @property
+    def radius(self):
+        pass 
+    
+    @property 
+    def diameter(self):
+        pass 
+    
+    @property
+    def circumference(self):
+        pass
+    
+    @property
+    def area(self):
+        pass
+    
