45406203b6b740d033f5a5615ceb779d23a89054
mossadhagos <155261758+mossadhagos@users.noreply.github.com>
2025-11-06 16:24:28 +0100

    Create rectangle.py

---
+++ python_lab_02/lab_2_python/rectangle.py
@@ -0,0 +1,26 @@
+
+import math
+from shape import Shape2d
+
+class Rectangle(Shape2d):
+    
+    def __init__(self, x: float = 0, y: float = 0, width: float = 1, height: float = 1):
+        super().__init__(x,y)
+        if not isinstance(width, (int, float)) or not isinstance(height,(int, float )):
+            raise TypeError("width and height must be positive")
+       
+        if width <= 0 or height <= 0:
+            raise ValueError("width and height must be postive")
+        self.width = float(width)
+        self.height = float(height)
+    
+    @property
+    def area(self) -> float:
+        return self.width * self.height
+    
+    @property
+    def perimeter(self) -> float:
+        return 2 * (self.width * self.height)
+    
+    def is_square(self) -> bool:
+        return self.width == self.height
