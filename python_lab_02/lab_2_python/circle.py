73828863a733cee5deabfc0e00b8633a8702ffba
mossadhagos <155261758+mossadhagos@users.noreply.github.com>
2025-11-06 16:24:23 +0100

    Create circle.py

---
+++ python_lab_02/lab_2_python/circle.py
@@ -0,0 +1,26 @@
+import math 
+from shape import Shape2d
+
+class Circle(Shape2d):
+    
+    def __init__(self, x: float = 0, y: float = 0, radius: float = 1 ):
+        super().__init__(x, y) 
+        
+        if not isinstance(radius, (int, float )):
+            raise TypeError ("radius must be a number")
+        
+        if radius <= 0:
+            raise ValueError("radius must be positive")
+        self.radius = float(radius)
+        
+    
+    @property
+    def area(self) -> float:
+        return math.pi * self.radius**2
+    
+    @property
+    def perimeter(self) -> float:
+        return 2 * math.pi * self.radius
+    
+    def is_unit(self) -> bool:
+        return self.radius == 1
