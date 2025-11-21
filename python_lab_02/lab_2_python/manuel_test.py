76d7a5eada186d0a51df60e379b2171f0772f271
mossadhagos <155261758+mossadhagos@users.noreply.github.com>
2025-11-21 21:15:12 +0100

    kompliterad

--- python_lab_02/lab_2_python/manuel_test.py
+++ python_lab_02/lab_2_python/manuel_test.py
@@ -7,12 +7,10 @@ from rectangle import Rectangle
 
 circle1 = Circle(0, 0, 1)
 circle2 = Circle(1, 1, 1)
-rectangle = Rectangle(0, 0, 1, 1)
+rectangle = Rectangle(0, 0, 1, 2)
 
 print(circle1 == circle2) # True, same area
 print(circle1 == rectangle) # False
 circle1.translate(5, 3)
 circle3 = Circle(radius=3)
 print(circle3 >= circle1) # True
-
-
--- python_lab_02/lab_2_python/rectangle.py
+++ python_lab_02/lab_2_python/rectangle.py
@@ -1,38 +1,30 @@
-# jag impoterar abstract klassen Shape2D
 from shape import Shape2D
 
-# jag skappar klassen rectangle
 class Rectangle(Shape2D):
+    """
+    Definierar en rektangel som en konkret 2D-form med position, bredd och höjd.
+    Validerar dimensioner, exponerar geometriska egenskaper och kontrollerar om
+    formen är en kvadrat.
+    """
     
-    # här har vi en dudder init
-    # med klass beskrinving 
     def __init__(self, x: float = 0, y: float = 0, width: float = 1, height: float = 1):
-       
-       # här hämtar/roppar på instroktioner från huvud klassen Shape2D
         super().__init__(x,y)
-        
-        # här varifierar vi att bredden och höjden är nummer 
         if not isinstance(width, (int, float)) or not isinstance(height,(int, float )):
-            raise TypeError("width and height must be positive")
-       
-       # här säger vi till att både höjden och bredden är större än 0
+            raise TypeError("width and height must be numbers")
+    
         if width <= 0 or height <= 0:
             raise ValueError("width and height must be postive")
-        
-        # förvarar höjd och bredd som float
         self.width = float(width)
         self.height = float(height)
     
-   # detta räknar arean av rectangle A = width * height
+  
     @property
     def area(self) -> float:
         return self.width * self.height
-    
-   # detta räknar perimeter p = 2 * (width + height)
+
     @property
     def perimeter(self) -> float:
         return 2 * (self.width * self.height)
     
-   # detta svarar True om höjd och bred är samma, och False om det inte är samma 
     def is_square(self) -> bool:
         return self.width == self.height
